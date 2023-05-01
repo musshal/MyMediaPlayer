cb933170f58e02071d88512b3e4d93da228d2450
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-01 13:35:41 +0700

    create: initial commit

--- app/src/main/java/com/dicoding/mymediaplayer/MainActivity.kt
+++ app/src/main/java/com/dicoding/mymediaplayer/MainActivity.kt
@@ -1,63 +1,81 @@
 package com.dicoding.mymediaplayer
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.os.Message
+import android.os.Messenger
+import android.os.RemoteException
+import android.util.Log
 import android.widget.Button
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-    private var mMediaPlayer: MediaPlayer? = null
-    private var isReady: Boolean = false
+    private lateinit var mBoundServiceIntent: Intent
+
+    private var mService: Messenger? = null
+    private var mServiceBound = false
+    private val mServiceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+            mService = null
+            mServiceBound = false
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            mService = Messenger(service)
+            mServiceBound = true
+        }
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mBoundServiceIntent = Intent(this@MainActivity, MediaService::class.java)
+        mBoundServiceIntent.action = MediaService.ACTION_CREATE
+
+        startService(mBoundServiceIntent)
+        bindService(mBoundServiceIntent, mServiceConnection, Context.BIND_AUTO_CREATE)
+
         val btnPlay = findViewById<Button>(R.id.btn_play)
         val btnStop = findViewById<Button>(R.id.btn_stop)
         btnPlay.setOnClickListener {
-            if (!isReady) {
-                mMediaPlayer?.prepareAsync()
-            } else {
-                if (mMediaPlayer?.isPlaying as Boolean) {
-                    mMediaPlayer?.pause()
-                } else {
-                    mMediaPlayer?.start()
+            if (mServiceBound) {
+                try {
+                    mService?.send(Message.obtain(null, MediaService.PLAY, 0, 0))
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
                 }
             }
         }
         btnStop.setOnClickListener {
-            if (mMediaPlayer?.isPlaying as Boolean || isReady) {
-                mMediaPlayer?.stop()
-                isReady = false
+            if (mServiceBound) {
+                try {
+                    mService?.send(Message.obtain(null, MediaService.STOP, 0, 0))
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
+                }
             }
         }
-
-        init()
     }
 
-    private fun init() {
-        mMediaPlayer = MediaPlayer()
-        val attribute = AudioAttributes.Builder()
-            .setUsage(AudioAttributes.USAGE_MEDIA)
-            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-            .build()
-        mMediaPlayer?.setAudioAttributes(attribute)
-        val afd = applicationContext.resources.openRawResourceFd(R.raw.guitar_background)
-        try {
-            mMediaPlayer?.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+        unbindService(mServiceConnection)
+        mBoundServiceIntent.action = MediaService.ACTION_DESTROY
 
-        mMediaPlayer?.setOnPreparedListener {
-            isReady = true
-            mMediaPlayer?.start()
-        }
-
-        mMediaPlayer?.setOnErrorListener { _, _, _ -> false }
+        startService(mBoundServiceIntent)
     }
 }
